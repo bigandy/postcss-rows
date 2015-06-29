1dc64c1f19bbe9821f9ececa17abee8e9cc289a3
Andrew Hudson <ahudson@gmail.com>
2015-06-29 20:11:26 +0100

    set default unit to be rows, remove some code that was not required

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 var postcss = require('postcss');
 
 /**
- * Convert unit to unitless px value.
- * @param  {Number} value
- * @param  {String} unit
- * @return {Number}
- */
-var toPx = function (value, unit) {
-	if (unit === 'em' || unit === 'rem') {
-		return parseFloat(value) * 16;
-	} else if (unit === '%') {
-		return parseFloat(value) / 100 * 16;
-	}
-
-	// This will be a px value, thanks to strict regex.
-	return value;
-};
-
-/**
  * Gets the multiplier value.
  * @param  {Number} declValue
  * @return {Number}
@@ -31,7 +14,7 @@ var valueMultiplier = function (declValue, multiplier) {
 module.exports = postcss.plugin('postcss-rows', function (opts) {
 	opts = opts || {};
 	var multiplier = opts.multiplier || '16';
-	var units = opts.units || 'vr';
+	var units = opts.units || 'rows';
 
   	return function (css) {
 		css.eachDecl(function transformDecl (decl) {
@@ -40,10 +23,8 @@ module.exports = postcss.plugin('postcss-rows', function (opts) {
 			// var regexp = new RegExp('/rows\([0-9]*.\)/gim');
 			var regexp = new RegExp('(\\d*\\.?\\d+)' + units, 'gi');
 
-
 			// Replace each vr unit value in the decl.value, e.g. shorthand properties.
 			decl.value = decl.value.replace(regexp, function ($1) {
-				// console.log($1);
 				return valueMultiplier($1, multiplier) + 'px';
 			});
     	});
